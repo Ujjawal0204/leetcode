701baf82a842f04366b3af78f35b5b942cd17f49
Ujjawal jaiswal <91370904+Ujjawal0204@users.noreply.github.com>
2023-10-29 23:36:19 +0530

    Time: 93 ms (21.95%), Space: 60.1 MB (15.42%) - LeetHub

---
+++ 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int start=0, end= arr.size()-1, mid;
+        while(start<end){
+            mid= start+(end-start)/2;
+            if(arr[mid]<arr[mid+1]){
+                start=mid+1;
+            }
+            else {
+                end=mid;
+            }
+        }
+        return start;
+    
+    }
+};
