e2f6b919f4cb65666d54678df6e4f43492f6712c
Ujjawal jaiswal <91370904+Ujjawal0204@users.noreply.github.com>
2024-01-18 11:05:01 +0530

    Time: 2 ms (45.07%) | Memory: 6.3 MB (71.72%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+
+    int climbStairs(int n) {
+        
+        int a =0;
+        int b=1;
+        int c=0;
+        for(int i=0;i<n;i++){
+            c = a+b;
+            a=b;
+            b=c;
+        }
+        return b;
+    }
+};
